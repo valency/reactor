97b51cb937db2cc1a6d1369d68e6e8b26cae4ae1
Ye Ding <guiewy@gmail.com>
2015-11-29 22:03:36 +0800

    v0.11.29

--- reactor.php
+++ reactor.php
@@ -27,14 +27,14 @@
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                     <i class="fa fa-server"></i>
                     <span id="server-label-ui">SmartCube UI Server</span><br/>
-                    <a id="server-url-ui" href="http://<?php echo $_SERVER["SERVER_NAME"]; ?>/joker/" target="_blank">http://<?php echo $_SERVER["SERVER_NAME"]; ?>/joker/</a><br/>
+                    <a id="server-url-ui" proxy="http://<?php echo $_SERVER["SERVER_NAME"] . ":8080"; ?>/joker/" href="http://<?php echo $_SERVER["SERVER_NAME"]; ?>/joker/" target="_blank">http://<?php echo $_SERVER["SERVER_NAME"]; ?>/joker/</a><br/>
                     <span id="server-status-ui" class="label label-default">Testing ...</span>
                     <a id="server-op-ui" href="javascript:void(0)" class="label label-default"> - </a>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                     <i class="fa fa-server"></i>
                     <span id="server-label-api">SmartCube API Server</span><br/>
-                    <a id="server-url-api" proxy="https://<?php echo $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"]; ?>/admin/" href="https://<?php echo $_SERVER["SERVER_NAME"]; ?>/admin/" target="_blank">https://<?php echo $_SERVER["SERVER_NAME"]; ?>/admin/</a><br/>
+                    <a id="server-url-api" proxy="https://<?php echo $_SERVER["SERVER_NAME"] . ":8443"; ?>/admin/" href="https://<?php echo $_SERVER["SERVER_NAME"]; ?>/admin/" target="_blank">https://<?php echo $_SERVER["SERVER_NAME"]; ?>/admin/</a><br/>
                     <span id="server-status-api" class="label label-default">Testing ...</span>
                     <a id="server-op-api" href="javascript:void(0)" class="label label-default"> - </a>
                 </div>
